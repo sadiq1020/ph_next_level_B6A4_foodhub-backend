5e3479cce545c6ecf620b5a845ac6f17ea79504b
Md. Sadiq Ibn Mas-Ud <sajidnsubd@gmail.com>
2026-02-23 05:12:30 +0600

    again changed to /auth only

--- src/app.ts
+++ src/app.ts
@@ -110,7 +110,7 @@ app.use(
 
 app.use(express.json());
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use("/api/auth", toNodeHandler(auth));
 
 // all custom routes
 app.use("/meals", mealRouter);
