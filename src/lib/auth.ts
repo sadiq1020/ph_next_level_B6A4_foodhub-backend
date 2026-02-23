0cef572ae422fa74b0716c0b05f041a67fbad5b5
Md. Sadiq Ibn Mas-Ud <sajidnsubd@gmail.com>
2026-02-23 06:41:48 +0600

    Add Vercel URL to trusted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,36 +2,36 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
-
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-    trustedOrigins: [
-        process.env.APP_URL!, // http://localhost:3000
-        "http://localhost:3000"
-    ],
-    user: {
-        additionalFields: {
-            role: {
-                type: "string",
-                defaultValue: "CUSTOMER",
-                input: true, // Allow setting during registration
-            },
-            phone: {
-                type: "string",
-                required: false,
-                input: true,
-            },
-            isActive: {
-                type: "boolean",
-                defaultValue: true,
-            },
-        },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://ph-next-level-b6-a4-foodhub-fronten.vercel.app", // Vercel URL
+    process.env.APP_URL!, // http://localhost:3000
+  ],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "CUSTOMER",
+        input: true, // Allow setting during registration
+      },
+      phone: {
+        type: "string",
+        required: false,
+        input: true,
+      },
+      isActive: {
+        type: "boolean",
+        defaultValue: true,
+      },
     },
-    emailAndPassword: {
-        enabled: true,
-        autoSignIn: false,
-        requireEmailVerification: false
-    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: false,
+  },
 });
